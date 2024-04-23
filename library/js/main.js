e77762ae24377b4097e9b825fe57eeae7ac2dd4a
Obie Silva <obiesilva@gmail.com>
2024-04-23 16:05:25 -0400

    Do not merge - New form updates.

--- library/js/main.js
+++ library/js/main.js
@@ -5,82 +5,58 @@ $(window).on("load scroll resize", function() {
     $('body').height(windowH);
 });
 
-
 /**** FORM VARS ****/
-var newUserBtn = $('#new-user');
-var newUserToggle = $('#field150832072_1');
-var rUserBtn = $('#returning-user');
-var rUserToggle = $('#field150832072_2');
-var newEmail = $('#email_address');
+var newEmail = $('#sf_email_address');
+var formEmail = $('#field163865448');
+var formEmailWrapper = $('#label-field163865448')
+var firstSection = $('#fsSection163865444');
+var accessSection = $('#fsSection163865452');
+var yourInfoSection = $('#fsSection163865545');
+var orgInfoSection = $('#fsSection163865567');
+var submit = $('#fsSubmitButton5728648');
 
-var formEmail = $('#field150832089');
-var formEmailV = formEmail.val();
-var orgF = $('#field149752909');
-var orgV = orgF.val();
-var fnF = $('#field149752881-first');
-var fnV = fnF.val();
-var lnF = $('#field149752881-last');
-var lnV = lnF.val();
-var roleF = $('#field150832190');
-var roleV = roleF.val();
-var buseF = $('#field149752996');
-var buseV = buseF.val();
-var phoneF = $('#field149753000');
-var phoneV = phoneF.val();
-var dataF = $('#field149753014');
-var dataV = dataF.val();
-var orgTypeF = $('#field149752908');
-var orgTypeV = orgTypeF.val();
-var countryF = $('#field149752948');
-var countryV = countryF.val();
-var zipF = $('#field149752986');
-var zipV = zipF.val();
+// Element where the buttons should be added
+const formFooter = document.getElementById('fsSubmit5728648');
 
-var firstSection = $('#fsSection151244438');
-var accessSection = $('#fsSection150832092');
-var yourInfoSection = $('#fsSection149754138');
-var orgInfoSection = $('#fsSection149754158');
-var submit = $('#fsSubmitButton5408173');
+// 'Next' button
+const nextButton = document.createElement('button');
+nextButton.id = 'fsNextButton5408173';
+nextButton.className = 'fsNextButton';
+nextButton.type = 'button';
+nextButton.value = 'Next Page';
+nextButton.setAttribute('aria-label', 'Next');
+nextButton.innerHTML = '<span class="fsFull">Next</span>';
 
-var cookiesPresent = 0;
+// 'Previous' button
+const previousButton = document.createElement('button');
+previousButton.id = 'fsPreviousButton5408173';
+previousButton.className = 'fsPreviousButton';
+previousButton.type = 'button';
+previousButton.value = 'Previous Page';
+previousButton.setAttribute('aria-label', 'Previous');
+previousButton.innerHTML = '<span class="fsFull">Previous</span>';
 
+// Append the buttons to the form footer
+formFooter.appendChild(previousButton);
+formFooter.appendChild(nextButton);
 
-/**** REPLACE FS FORM ELEMENTS ****/
-$(newUserBtn).on('click', function(){
-    $('.fsNextButton').show();
-    $(newUserToggle).prop("checked", true); 
-});
-$(rUserBtn).on('click', function(){
-    $('.fsNextButton').hide();
-    $(rUserToggle).prop("checked", true); 
-});
-$(newEmail).keyup(function(){
+// Email Validation
+function validateEmail(email) {
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+}
+
+$(newEmail).on('input', function(){
     email = $(this).val();
     $(formEmail).val(email);
-    $(this).removeClass('warning');
-    $('.user-choice-btn').attr('data-bs-target', '#questionnaire');
-});
 
-/**** FORM NAVIGATION ****/
-// Force user to select new or returning; require email to proceed
-$('.user-choice-btn').on('click', function(){
-    checkForCookies();
-    if( $(newEmail).val().length === 0 ){
-        $(newEmail).addClass('warning');
-    }else if( cookiesPresent === 0){
-        // show the form
-        $('.fsNextButton').show();
-        $(orgF).add(fnF).add(lnF).add(roleF).add(buseF).add(phoneF).add(dataF).add(orgTypeF).add(countryF).add(zipF).prop('required', true).addClass('fsRequired');
-        console.log('no cookies, show the form');
-    }else if( cookiesPresent === 1){
-        // submit form
-        console.log('found some cookies, submit the form');
-        $('#redirecting-msg').show();
-        $(rUserToggle).prop("checked", true); 
-        $('#fsForm5408173').submit();
-        getCookieValue();
-        setReturningUserCookie();
-
+    console.log("Current email value: '" + email + "'");
+    if (email.length === 0) {
+        console.log("Email is empty, adding warning class.");
+        $(this).addClass('warning');
+    } else {
+        console.log("Email is not empty, removing warning class.");
+        $(this).removeClass('warning');
     }
 });
 
@@ -88,6 +64,9 @@ $('.user-choice-btn').on('click', function(){
 $(submit).appendTo('#hold');
 $(firstSection).hide();
 $(yourInfoSection).add(orgInfoSection).hide();
+$('.fsPreviousButton').hide();
+$(submit).hide();
+// $(formEmailWrapper).hide();
 
 // advancing the form with next and previous clicks
 $('.fsNextButton').on('click', function(){
@@ -96,6 +75,7 @@ $('.fsNextButton').on('click', function(){
     $(submit).prependTo('.fsSubmit');
     $('.fsPreviousButton').show();
     $(yourInfoSection).add(orgInfoSection).show();
+    $(submit).show();
 });
 
 $('.fsPreviousButton').on('click', function(){
@@ -103,17 +83,14 @@ $('.fsPreviousButton').on('click', function(){
     $(accessSection).show();
     $('.fsNextButton').show();
     $(yourInfoSection).add(orgInfoSection).hide();
+    $(submit).hide();
 });
-
-// Email Validation
-function validateEmail(email) {
-    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailPattern.test(email);
-}
 
 // POST call to Salesforce Database Endpoint
 $('#submitEmail').on('click', function(){
-    var email = document.getElementById('sf_email_address').value;
+    $(newEmail).trigger('input');
+    var email = $(newEmail).val();
+
     if (email && validateEmail(email)) {
         $('#result').html("");  // Clear any error messages
         $.ajax({
@@ -126,13 +103,10 @@ $('#submitEmail').on('click', function(){
                     $('#result').html("Please wait while we redirect you to our database ...");
                     window.location.href = 'https://allergyasthmanetwork.shinyapps.io/asthma-dashboard-v3-main/';
                 } else if (response === "Email does not exist in Salesforce DB") {
-                    // Trigger the modal only if email does not exist in Salesforce DB
-                    // $('.submit-btn').attr('data-bs-target', '#questionnaire');
                     $('#questionnaire').modal('show'); 
                     $(firstSection).show();
                     $('.fsNextButton').show(); 
                 } else {
-                    // Handle other unexpected responses or log them for further analysis
                     console.error("Unexpected response:", response);
                     $('#result').html("Unexpected error occurred. Please refresh the page and try again.");
                 }
@@ -146,95 +120,6 @@ $('#submitEmail').on('click', function(){
     }
 });
 
-/**** COOKIE SETUP ****/
-// Set up a cookie to store basic info
-function setReturningUserCookie() {
-    var expirationDate = new Date();
-    var user = document.getElementById('email_address').value;
-    var fn = document.getElementById('field149752881-first').value;
-    var ln = document.getElementById('field149752881-last').value;
-    var org = document.getElementById('field149752909').value;
-    
-    var aeeRU = encodeURIComponent(user);
-    var aeeRUfn = encodeURIComponent(fn);
-    var aeeRUln = encodeURIComponent(ln);
-    var aeeRUorg = encodeURIComponent(org);
-
-    var expirationDate = new Date();
-    expirationDate.setFullYear(expirationDate.getFullYear() + 10);
-    var expiresString = "expires=" + expirationDate.toUTCString();
-
-    console.log("Email: "+aeeRU+", FN: "+aeeRUfn+", LN: "+aeeRUln+", ORG: "+aeeRUorg);
-
-    document.cookie = "aeeRU=" + aeeRU + "; " + expiresString + "; path=/";
-    document.cookie = "aeeRUfn=" + aeeRUfn + "; " + expiresString + "; path=/";
-    document.cookie = "aeeRUln=" + aeeRUln + "; " + expiresString + "; path=/";
-    document.cookie = "aeeRUorg=" + aeeRUorg + "; " + expiresString + "; path=/";
-}
-
-// Check Cookies
-function checkForCookies() {
-    // Function to get the value of a specific cookie by name
-    function getCookieValue(cookieName) {
-        var name = cookieName + "=";
-        var decodedCookie = decodeURIComponent(document.cookie);
-        var ca = decodedCookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) === ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) === 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
-    }
-
-    // Check for each cookie
-    var aeeRU = getCookieValue('aeeRU');
-    var aeeRUfn = getCookieValue('aeeRUfn');
-    var aeeRUln = getCookieValue('aeeRUln');
-    var aeeRUorg = getCookieValue('aeeRUorg');
-
-    if (aeeRU && aeeRUfn && aeeRUln && aeeRUorg) {
-        console.log("Found cookies: ", { aeeRU, aeeRUfn, aeeRUln, aeeRUorg });
-
-        newEmail.val(aeeRU);
-        formEmail.val(aeeRU);
-        orgF.val(aeeRUorg);
-        fnF.val(aeeRUfn);
-        lnF.val(aeeRUln);
-
-        cookiesPresent = 1;
-        return { aeeRU, aeeRUfn, aeeRUln, aeeRUorg };
-
-    } else {
-        // console.log("No relevant cookies found");
-         cookiesPresent = 0;
-        // return null;
-    }
-}
-
-/**** VALIDATE & SUBMIT ****/
-// Submit & Store the Cookies
 $(submit).on('click', function(){
-    if( $('#email_address').val().length === 0 ){
-        $(newEmail).addClass('warning');
-        // do not update cookie
-    }else{
-        // check for cookies
-        if( cookiesPresent === 1){
-            // submit form
-            // console.log('found some cookies, submit the form');
-            $('#redirecting-msg').show();
-            $('#fsForm5408173').submit();
-            getCookieValue();
-        }else if( cookiesPresent === 0){
-            // require form entry & set the cookie
-            // console.log('no cookies at final validation check');
-            setReturningUserCookie();
-
-        }
-    }
+    console.log("Email at submission:", $(formEmail).val());
 });
--- partials/modal-form.php
+++ partials/modal-form.php
@@ -5,8 +5,7 @@
 			<div class="text-end" >
 				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 			</div>
-			<script type="text/javascript" src="https://allergyasthmanetwork.formstack.com/forms/js.php/aee"></script><noscript><a href="https://allergyasthmanetwork.formstack.com/forms/aee" title="Online Form">Online Form - Asthma Equity Explorer Database</a></noscript>
-
+				<script type="text/javascript" src="https://allergyasthmanetwork.formstack.com/forms/js.php/asthma_equity_explorer_database_registration"></script><noscript><a href="https://allergyasthmanetwork.formstack.com/forms/asthma_equity_explorer_database_registration" title="Online Form">Online Form - Asthma Equity Explorer Database Registration</a></noscript>
 			<div id="redirecting-msg" class="text-center" >
 				<h2>Thank you!</h2>
 				<p>You are now being redirected to the database.</p>
